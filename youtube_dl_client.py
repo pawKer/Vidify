d00932fe22cca34603dd3ad51e2731b33e9da785
Rares <rares.rck96@yahoo.ro>
2020-11-10 21:44:19 +0000

    Youtube-dl now tries to find video 3 times

--- youtube_dl_client.py
+++ youtube_dl_client.py
@@ -5,19 +5,28 @@ log = logging.getLogger('flask_web_server.service')
 class YoutubeDlClient(YoutubeClient):
 
     def get_youtube_link(self, song_title, artist):
-        with youtube_dl.YoutubeDL() as ytdl:
-            try:
-                log.info(song_title)
-                data = ytdl.extract_info("ytsearch:" + song_title + " " + artist, download=False)
-            except Exception as e:
-                # Any kind of error has to be caught, so that it doesn't only
-                # send the error signal when the download wasn't successful
-                # (a DownloadError from youtube_dl).
-                log.warning("YouTube-dl wasn't able to obtain the video: %s",
-                             str(e))
-            else:
-                if len(data['entries']) == 0:
-                    log.warning("YouTube-dl returned no entries")
-                    return self.DEFAULT_VIDEO_ID
+        youtube_id = None
+        tries = 0
+        while youtube_id == None and tries < 3:
+            with youtube_dl.YoutubeDL() as ytdl:
+                try:
+                    log.info("Attempt number " + str(tries + 1))
+                    data = ytdl.extract_info("ytsearch:" + song_title + " " + artist, download=False)
+                except Exception as e:
+                    # Any kind of error has to be caught, so that it doesn't only
+                    # send the error signal when the download wasn't successful
+                    # (a DownloadError from youtube_dl).
+                    log.warning("YouTube-dl wasn't able to obtain the video: %s",
+                                str(e))
                 else:
-                    return data['entries'][0]['id']
+                    if len(data['entries']) == 0:
+                        log.warning("YouTube-dl returned no entries")
+                    else:
+                        youtube_id = data['entries'][0]['id']
+                        break
+            tries = tries + 1
+        if youtube_id == None:
+            log.warning("Could not find youtube video after  " + str(tries) + " attempts. Using default.")
+            return self.DEFAULT_VIDEO_ID
+        else:
+            return youtube_id
